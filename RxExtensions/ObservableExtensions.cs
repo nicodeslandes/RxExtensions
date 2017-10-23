2583286c7497e91989d231de6b360b259fea1a0e
Nicolas Deslandes <ndeslandes@live.fr>
2017-10-23 11:48:57 +0100

    Base implementation of DelayedRefCount: no delay

---
+++ RxExtensions/ObservableExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+public static class ObservableExtensions
+{
+    public static IObservable<T> DelayedRefCount<T>(this IConnectableObservable<T> connectable, TimeSpan delay)
+    {
+        var sync = new object();
+        var count = 0;
+        IDisposable connection = null;
+        return Observable.Create<T>(obs =>
+        {
+            lock (sync)
+            {
+                if (count++ == 0)
+                {
+                    // First subscriber; need to connect the connectable
+                    // TODO: Move out of the lock?
+                    connection = connectable.Connect();
+                }
+            }
+
+            var subscription = connectable.Subscribe(obs);
+
+            void UnsubscribeObserver()
+            {
+                subscription.Dispose();
+                lock (sync)
+                {
+                    if (--count == 0)
+                    {
+                        // Last subscriber
+                        connection?.Dispose();
+                        connection = null;
+                    }
+                }
+            }
+
+            return Disposable.Create(UnsubscribeObserver);
+        });
+    }
+
+    public static IObservable<T> OnSubscribe<T>(this IObservable<T> src, Action action)
+    {
+        return Observable.Create<T>(obs =>
+        {
+            action();
+            return src.Subscribe(obs);
+        });
+    }
+    public static IObservable<T> OnUnsubscribed<T>(this IObservable<T> src, Action action)
+    {
+        return Observable.Create<T>(obs => new CompositeDisposable(
+            src.Subscribe(obs),
+            Disposable.Create(action)));
+    }
+}
--- RxExtensions/Program.cs
+++ RxExtensions/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
 
 namespace RxExtensions
 {
@@ -6,7 +8,43 @@ namespace RxExtensions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var src = Observable.Interval(TimeSpan.FromSeconds(1))
+                .OnSubscribe(() => Console.WriteLine("New subscriber"))
+                .OnUnsubscribed(() => Console.WriteLine("Unsubscribed"))
+                .Do(i => Console.WriteLine($"New item: {i}"));
+
+            var refCounted = src
+                .Publish()
+                .DelayedRefCount(TimeSpan.FromSeconds(5));
+            //.RefCount();
+
+            Console.WriteLine("Starting S1");
+            var s1 = refCounted.Subscribe(i => Console.WriteLine($"S1: {i}"));
+
+            Thread.Sleep(3000);
+            Console.WriteLine("Starting S2");
+            var s2 = refCounted.Subscribe(i => Console.WriteLine($"S2: {i}"));
+
+            Thread.Sleep(4000);
+            Console.WriteLine("Stopping S1");
+            s1.Dispose();
+
+            Thread.Sleep(4000);
+            Console.WriteLine("Stopping S2");
+            s2.Dispose();
+            Console.WriteLine("S2 stopped");
+            Thread.Sleep(4000);
+
+            Console.WriteLine("Starting S3");
+            var s3 = refCounted.Subscribe(i => Console.WriteLine($"S3: {i}"));
+            Thread.Sleep(4000);
+            Console.WriteLine("Stopping S3");
+            s3.Dispose();
+            Console.WriteLine("S3 stopped");
+            Thread.Sleep(4000);
+
+            Console.WriteLine("Press Enter to quit");
+            Console.ReadLine();
         }
     }
 }
